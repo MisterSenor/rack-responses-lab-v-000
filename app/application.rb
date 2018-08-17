d97564edbbfcffb63955af7009e66f28cd32b3c9
Nathan  Visak <nathan.visak@gmail.com>
2018-08-17 16:25:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.now
-    if time <= 12
+    if time <= (2018,8,17,11,59)
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
